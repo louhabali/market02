5e524ef202edf360759068381302d5ae123bd469
iichi <ismailichi29@gmail.com>
2026-07-28 16:33:53 +0100

    modify the jenkins file : seperate tests & packaging 5

--- gateway_service/src/test/java/buy01/gateway_service/GatewayServiceApplicationTests.java
+++ gateway_service/src/test/java/buy01/gateway_service/GatewayServiceApplicationTests.java
@@ -2,8 +2,10 @@ package buy01.gateway_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+
 class GatewayServiceApplicationTests {
 
 	@Test
